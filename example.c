1e6f656ef7654b5c1f830e2341af289dda3ef496
Piotr Oleskiewicz <piotr@oleskiewi.cz>
2019-05-12 14:26:18 +0100

    fix example and documentation

--- example.c
+++ example.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 #define KV \
-	X(char *, out, strdup, "%s", ".") \
-	X(double, Om, atof, "%f", 0.0) \
-	X(double, Ol, atof, "%f", 0.0) \
-	X(int, nout, atoi, "%d", 1) \
-	X(bool, flat, atob, "%d", true)
+	X(char *, out, ".") \
+	X(double, Om, 0.0) \
+	X(double, Ol, 0.0) \
+	X(int, nout, 1) \
+	X(bool, flat, true)
 
 #include "kv.h"
 
